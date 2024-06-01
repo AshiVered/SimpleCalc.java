d406f38f828b28b152ea2bd7003025567a3a4915
Ashi Vered <76535872+AshiVered@users.noreply.github.com>
2024-06-02 00:28:07 +0300

    1.1
    Prevention of repetition and further corrections

--- LearnLang/src/Main.java
+++ LearnLang/src/Main.java
@@ -1,7 +1,7 @@
 /* LearnLang.java
    Author: Ashi Vered
-   Version: 1.0
-   Date: 23.8.5784
+   Version: 1.1
+   Date: 25.9.5784
  */
 
 import java.util.Random;
@@ -10,41 +10,32 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         int score = 0;
+        int num_words = 19;
+        int num_questions = 10;
         Scanner in = new Scanner(System.in);
         Random r = new Random();
+        //array to mark which positions are used
+        boolean[] used = new boolean[20];
+
+
 
         //array for original words
-        String[] words_orig = new String[10];
-        //list of original words
-        words_orig[0] = "אבא";
-        words_orig[1] = "אמא";
-        words_orig[2] = "אח";
-        words_orig[3] = "אחות";
-        words_orig[4] = "סבא";
-        words_orig[5] = "סבתא";
-        words_orig[6] = "דוד";
-        words_orig[7] = "דודה";
-        words_orig[8] = "אחיין";
-        words_orig[9] = "אחיינית";
+        String[] words_orig = {"אבא", "אמא", "אח", "אחות", "סבא", "סבתא", "דוד", "דודה", "אחיין", "אחיינית", "יהלום", "חבר", "משפחה", "טוב", "רע", "אמת", "שקר", "מחשב", "יפה", "מכוער"};
 
         //array for new words
-        String[] words_new = new String[10];
-        //list of new words
-        words_new[0] = "father";
-        words_new[1] = "mother";
-        words_new[2] = "brother";
-        words_new[3] = "sister";
-        words_new[4] = "grandfather";
-        words_new[5] = "grandmother";
-        words_new[6] = "uncle";
-        words_new[7] = "aunt";
-        words_new[8] = "nephew";
-        words_new[9] = "niece";
+        String[] words_new =  {"father", "mother", "brother", "sister", "grandfather", "grandmother", "uncle", "aunt", "nephew", "niece", "diamond", "friend", "family", "good", "bad", "true", "false", "computer", "beautiful", "ugly"};
 
-        for (int i = 0; i < 10; i++) {
+
+        for (int i = 0; i < num_questions; i++) {
             int selectArray = r.nextInt(2); // Randomly decide whether to prompt for a translation from original lang to new lang.
-            int position = r.nextInt(9); // Randomly choose a position in the arrays (i.e. choose which word to display).
+            int position; // Randomly choose a position in the arrays (i.e. choose which word to display).
             String answer;
+            // check if position are used
+            do {
+                position = r.nextInt(num_words);
+            } while (used[position]);
+            used[position] = true;
+
                 if (selectArray == 0) {
                     // show word in original language.
                     String word_orig = words_orig[position];
@@ -75,10 +66,11 @@ public class Main {
                     }
                 }
             }
-                System.out.println("Your score: " + score + "/10");
+        System.out.println("Your score: " + score + "/10");
 
 
 
         in.close();
+        }
+
     }
-}
